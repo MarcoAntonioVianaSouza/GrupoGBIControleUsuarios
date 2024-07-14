d22a808ec96f95d811d155e1b75c71b1b9143054
marcoantonio <marcoantonio.parceiro@dnitservices.com>
2024-07-14 17:43:46 -0300

    Criando Mapeamento Inicial para Usuario (Utilizando FluentAPI).

---
+++ GrupoGBIControleUsuarios.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GrupoGBIControleUsuarios.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace GrupoGBIControleUsuarios.Infra.Data.Context;
+public class ApplicationDbContext : DbContext 
+{
+    // Através do DBContext teremos uma sessão com o banco de dados
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+
+    // Definição do mapeamento ORM
+    public DbSet<Usuario> Usuarios { get; set; }
+    
+
+    // Configuração da Fluent-API
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Estamos sobrescrevendo o método.
+        //Através de Reflection irá buscar quem implementa IEntityTypeConfiguration
+        //Destaa forma não precisamos especificar aqui neste método cada uma das configurações, pois
+        //ele irá identificar nos respectivos arquivos (classes de configuração de cada entidade do modelo).
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ GrupoGBIControleUsuarios.Infra.Data/EntitiesConfiguration/UsuarioConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GrupoGBIControleUsuarios.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(k => k.Id);
+            builder.Property(p => p.NomeDeUsuario).HasMaxLength(20).IsRequired().HasColumnType("varchar");
+            builder.Property(p => p.Nome).HasMaxLength(50).IsRequired().HasColumnType("varchar");
+            builder.Property(p => p.Sobrenome).HasMaxLength(50).IsRequired().HasColumnType("varchar");
+            builder.Property(p => p.Senha).HasMaxLength(255).IsRequired().HasColumnType("varchar");
+            builder.Property(p => p.Email).HasMaxLength(255).IsRequired().HasColumnType("varchar");
+            builder.Property(p => p.EAdministrador);
+        }
+    }
+}
