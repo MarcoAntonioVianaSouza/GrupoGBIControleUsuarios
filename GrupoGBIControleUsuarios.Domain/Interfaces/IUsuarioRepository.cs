11a6c662a09ef0ebef5625aaab1e763aaf3fe5b7
marcoantonio <marcoantonio.parceiro@dnitservices.com>
2024-07-14 17:23:05 -0300

    Criação Interface IUsuarioRepository.

---
+++ GrupoGBIControleUsuarios.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using GrupoGBIControleUsuarios.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces;
+
+public interface IUsuarioRepository
+{
+    Task<IEnumerable<Usuario>> ObterUsuarios();
+    Task<Usuario> ObterPorId(int? id);
+    Task<Usuario> Criar(Usuario usuario);
+    Task<Usuario> Atualizar(Usuario usuario);
+    Task<Usuario> Remover(Usuario usuario);
+}
