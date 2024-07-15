4cddf486d531185998e1094cb3a9bd5534e1af6e
marcoantonio <marcoantonio.parceiro@dnitservices.com>
2024-07-15 08:06:52 -0300

    Criação arquivo Docker Compose e Adição de valor Encrypt=False para conexão sqlserver.

--- GrupoGBIControleUsuarios.Infra.IoC/DependencyInjectionAPI.cs
+++ GrupoGBIControleUsuarios.Infra.IoC/DependencyInjectionAPI.cs
@@ -15,9 +15,21 @@ public static class DependencyInjectionAPI
     public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
         IConfiguration configuration)
     {
+        //"ControleUsuarioDB": "Data Source=.\\MSSQLSERVER2019;Initial Catalog=GrupoGBIControleUsuario;Integrated Security=True;Encrypt=False"
+
+        var server = configuration["DbServer"] ?? "localhost\\MSSQLSERVER2019";
+        var port = configuration["DbPort"] ?? "1433"; // Padrão SQL SERVER
+        var user = configuration["DbUser"] ?? "SA";
+        var password = configuration["Password"] ?? "Dinamarca@2020";
+        var database = configuration["Database"] ?? "GrupoGBIControleUsuarioProd";
+        var connectionString = $"Server={server}, {port}; Initial Catalog={database}; User ID={user}; Password={password}; Encrypt=False";
+
         services.AddDbContext<ApplicationDbContext>(options =>
-         options.UseSqlServer(configuration.GetConnectionString("ControleUsuarioDB"
-        ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+         options.UseSqlServer(connectionString, b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        //services.AddDbContext<ApplicationDbContext>(options =>
+        // options.UseSqlServer(configuration.GetConnectionString("ControleUsuarioDB"
+        //), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
         services.AddScoped<IUsuarioService, UsuarioService>();
         services.AddScoped<IUsuarioRepository, UsuarioRepository>();
